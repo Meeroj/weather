cd9d09dddad1630586f690e7afdc0c293d0b7cff
Meeroj <sirojiddinovmerojiddin@gmail.com>
2024-02-28 14:45:43 +0500

    add

---
+++ index.js
@@ -0,0 +1,62 @@
+function formatTime(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
+function formatDay(date) {
+  const dayArray = date.getDay();
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const day = days[dayArray];
+  return day;
+}
+
+const currentTime = document.querySelector("#current-time");
+let newCurrentTime = new Date();
+currentTime.innerHTML = formatTime(newCurrentTime);
+
+const currentDay = document.querySelector("#current-day");
+let newCurrentDay = new Date();
+currentDay.innerHTML = formatDay(newCurrentDay);
+
+function displayWeatherInfo(response) {
+  document.querySelector("#searched-city").innerHTML = response.data.name;
+  const temperature = Math.round(response.data.main.temp);
+  document.querySelector("#current-temperature").innerHTML = `${temperature}°`;
+  const humidity = response.data.main.humidity;
+  document.querySelector("#humidity").innerHTML = `${humidity}%`;
+  const windSpeed = Math.round(response.data.wind.speed);
+  document.querySelector("#wind").innerHTML = `${windSpeed}km/h`;
+  document.querySelector("#weather-type").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  const apiKey = "2b5fc755ac2ec59250868b5527df31c4";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(displayWeatherInfo);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  searchCity(city);
+}
+
+const searchBar = document.querySelector("#search-form");
+searchBar.addEventListener("submit", handleSubmit);
